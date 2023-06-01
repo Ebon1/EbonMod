d6579fc202e4f5171ddb6223e2aa0a33fd4cf50a
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-06-01 13:46:00 +1000

    Update IchorGlobScepter.cs

--- Items/Weapons/Magic/IchorGlobScepter.cs
+++ Items/Weapons/Magic/IchorGlobScepter.cs
@@ -83,5 +83,150 @@ namespace EbonMod.Items.Weapons.Magic
 	
 		
 	}
+	 public class IchorGlob : ModProjectile
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ichor Glob");
+
+
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 30;
+            Projectile.height = 30;
+            Projectile.aiStyle = 0;
+            Projectile.tileCollide = true;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Magic;
+
+            Projectile.timeLeft = 160;
+            Projectile.scale = 1f;
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.NavajoWhite;
+        }
+        public override void Kill(int timeLeft)
+        {
+
+
+            int radius = 150;
+
+            for (int i = 0; i < Main.npc.Length; i++)
+            {
+                NPC target = Main.npc[i];
+                if (target.active && !target.friendly && Vector2.Distance(Projectile.Center, target.Center) < radius)
+                {
+                    
+                    target.SimpleStrikeNPC(damage: 25, 0);
+                }
+            }
+
+            Player player = Main.player[Projectile.owner];
+            player.GetModPlayer<Screenshake>().SmallScreenshake = true;
+            for (int i = 0; i < 3; i++)
+            {
+                float speedX = Projectile.velocity.X * Main.rand.NextFloat(.46f, .8f) + Main.rand.NextFloat(-7f, 8f);
+                float speedY = Projectile.velocity.Y * Main.rand.Next(40, 70) * 0.01f + Main.rand.Next(-20, 21) * 0.4f;
+
+                Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.Center.X, Projectile.Center.Y, 4, 0, ModContent.ProjectileType<IchorGlobSmall>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.Center.X, Projectile.Center.Y, -4, 0, ModContent.ProjectileType<IchorGlobSmall>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.Center.X, Projectile.Center.Y, 0, 4, ModContent.ProjectileType<IchorGlobSmall>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                Vector2 speed = Main.rand.NextVector2CircularEdge(1f, 1f);
+                var d = Dust.NewDustPerfect(Projectile.Center, DustID.Ichor, speed * 10, Scale: 3f);
+                ;
+                d.noGravity = true;
+            }
+        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(BuffID.Ichor, 400);
+
+        }
+        public override bool? CanHitNPC(NPC target)
+        {
+            return !target.friendly;
+        }
+        public override void AI()
+        {
+            Projectile.rotation += 0.1f;
+            Projectile.velocity *= 0.95f;
+            Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.Blood, Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f, Scale: 1f);
+
+        }
+
+    }
+    public class IchorGlobSmall : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Ichor Glob Small");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 18;
+			Projectile.height = 18;
+
+			Projectile.aiStyle = 0;
+			Projectile.DamageType = DamageClass.Magic;
+			Projectile.friendly = true;
+			Projectile.hostile = false;
+			Projectile.ignoreWater = true;
+			Projectile.light = 0.2f;
+			Projectile.tileCollide = true;
+			Projectile.timeLeft = 80;
+			Projectile.penetrate = 1;
+			Projectile.alpha = 0;
+
+		}
+		public override void AI()
+		{
+
+			Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.Blood, Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f, Scale: 0.8f);
+			Lighting.AddLight(Projectile.position, 0.1f, 0.1f, 0.1f);
+			Lighting.Brightness(1, 1);
+            Projectile.rotation += 0.3f;
+            Projectile.velocity *= 0.97f;
+
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(BuffID.Ichor, 400);
+
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.NavajoWhite;
+        }
+
+        public override void Kill(int timeleft)
+		{
+
+
+
+         //   Gore.NewGore(Projectile.GetSource_Death(), Projectile.Center, Vector2.Zero, Mod.Find<ModGore>("LightbulbBulletGore1").Type, 1f);
+			
+			for (int i = 0; i < 50; i++)
+			{
+				Vector2 speed = Main.rand.NextVector2CircularEdge(0.5f, 0.5f);
+				var d = Dust.NewDustPerfect(Projectile.Center, DustID.Ichor, speed * 5, Scale: 1f);
+				;
+				d.noGravity = true;
+			}
+
+			SoundEngine.PlaySound(SoundID.NPCDeath11);
+		}
+	}
 }
 
