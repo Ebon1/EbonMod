57adf19f344264f4e731eccc9d4ab1fc825e28ce
Ebon1 <alikhaddam42@gmail.com>
2023-12-06 22:00:37 +0100

    WIP achievement system

---
+++ Achievements/EbonianAchievementSystem.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.UI;
+
+namespace EbonianMod.Achievements
+{
+    public class EbonianAchievementSystem : ModSystem
+    {
+        public UserInterface achievementUI;
+        EbonianAchievementUIState achievementUIState;
+        public override void Load()
+        {
+            achievementUIState = new();
+            achievementUI = new();
+            achievementUI.SetState(achievementUIState);
+        }
+        public override void UpdateUI(GameTime gameTime)
+        {
+            achievementUI?.Update(gameTime);
+            if (Main.mouseRight)
+                IngameFancyUI.OpenUIState(achievementUIState);
+        }
+        public struct AchievementTemplate
+        {
+            public string Text;
+            public string Subtext;
+            public AchievementTemplate(string text, string subtext)
+            {
+                Text = text; Subtext = subtext;
+            }
+        }
+        public static AchievementTemplate[] achievementTemplates = new AchievementTemplate[maxAchievements]
+        {
+            new("Nuclear Waste", "Defeat Hot Garbage."),
+            new("Who's laughing now?", "Defeat the vile conglomerate of The Corruption, Terrortoma."),
+            new("Seeing Red", "Defeat the unsightly aberration of The Crimson, Cecitior"),
+            new("Infernal Archeology", "Unleash Exol of Ignos' soul through weakening Inferos the Rock (idk lmfao)."),
+            new("Soul-Crushing", "Defeat Exol of Ignos"),
+            new("Too Spicy", "Anything is edible, the aftermath does not matter."),
+            new("Quit hitting yourself!", "Terrortoma was never the brightest corpse in the pile..."),
+            new("Djungelskog", "Acquire Djungelskog."),
+            new("Voidrunner", "Defeat 10 enemies at once using one time slip."),
+            new("Organ Harvest", "Acquire a sentient organ."),
+            new("Lore Accurate", "Defeat Inferos using the blade of Exol."),
+        };
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+        {
+            int resourceBarIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+            if (resourceBarIndex != -1)
+            {
+                layers.Insert(resourceBarIndex, new LegacyGameInterfaceLayer(
+                    "EbonianMod: Achievements",
+                    delegate
+                    {
+                        achievementUI.Draw(Main.spriteBatch, new GameTime());
+                        return true;
+                    },
+                    InterfaceScaleType.UI)
+                );
+            }
+        }
+        public const int maxAchievements = 11;
+        public static bool[] acquiredAchievement = new bool[maxAchievements];
+        public override void ClearWorld()
+        {
+            for (int i = 0; i < maxAchievements; i++)
+            {
+                acquiredAchievement[i] = false;
+            }
+        }
+        public override void SaveWorldData(TagCompound tag)
+        {
+            for (int i = 0; i < maxAchievements; i++)
+            {
+                if (acquiredAchievement[i])
+                    tag["EbonianAchievement" + i] = true;
+            }
+        }
+        public override void LoadWorldData(TagCompound tag)
+        {
+            for (int i = 0; i < maxAchievements; i++)
+            {
+                acquiredAchievement[i] = tag.ContainsKey("EbonianAchievement" + i);
+            }
+        }
+        public override void NetSend(BinaryWriter writer)
+        {
+            BitsByte flags1 = new BitsByte();
+            for (int i = 0; i < 8; i++)
+            {
+                flags1[i] = acquiredAchievement[i];
+            }
+            writer.Write(flags1);
+            BitsByte flags2 = new BitsByte();
+            for (int i = 8; i < maxAchievements; i++)
+            {
+                flags2[i] = acquiredAchievement[i - 8];
+            }
+            writer.Write(flags2);
+        }
+        public override void NetReceive(BinaryReader reader)
+        {
+            BitsByte flags1 = reader.ReadByte();
+            for (int i = 0; i < 8; i++)
+            {
+                acquiredAchievement[i] = flags1[i];
+            }
+            BitsByte flags2 = reader.ReadByte();
+            for (int i = 8; i < maxAchievements; i++)
+            {
+                acquiredAchievement[i] = flags2[i];
+            }
+        }
+    }
+}
---
+++ Achievements/EbonianAchievementUI.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.GameContent.UI.Elements;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.UI;
+using Terraria.UI.Chat;
+
+namespace EbonianMod.Achievements
+{
+    public class EbonianAchievement : UIPanel
+    {
+        public EbonianAchievement(int index, string text, string subtext, string texture = "Extras/Sprites/Achievements")
+        {
+            Index = index;
+            Text = text;
+            Subtext = subtext;
+            TexturePath = texture;
+
+            BackgroundColor = new Color(51, 69, 132) * 0.75f;
+            BorderColor = Color.Black * 0.75f;
+            Height.Set(82f, 0f);
+            Width.Set(0f, 1f);
+            PaddingTop = 8f;
+            PaddingLeft = 9f;
+            icon = new UIImageFramed(ModContent.Request<Texture2D>("EbonianMod/" + TexturePath, ReLogic.Content.AssetRequestMode.ImmediateLoad), new Rectangle(0, 64 * index, 64, 64));
+            Append(icon);
+        }
+        public UIImageFramed icon;
+        public int Index;
+        public bool locked;
+        public string Text;
+        public string Subtext;
+        public string TexturePath;
+        public override void Update(GameTime gameTime)
+        {
+            locked = !EbonianAchievementSystem.acquiredAchievement[Index];
+            base.Update(gameTime);
+        }
+        protected override void DrawSelf(SpriteBatch spriteBatch)
+        {
+            locked = !EbonianAchievementSystem.acquiredAchievement[Index];
+            base.DrawSelf(spriteBatch);
+            CalculatedStyle iconSize = icon.GetDimensions();
+            float width = GetInnerDimensions().Width - iconSize.Width + 1f;
+
+
+            Vector2 textScale = new(0.85f);
+            Vector2 pos = new Vector2(iconSize.X + iconSize.Width + 7f, GetInnerDimensions().Y - 2f);
+
+            Texture2D topTex = Helper.GetExtraTexture("Sprites/TopPanel");
+            Texture2D bottomTex = Helper.GetExtraTexture("Sprites/BottomPanel");
+            Color color = IsMouseHovering ? Color.White : Color.Gray;
+
+            spriteBatch.Draw(topTex, pos, new Rectangle(0, 0, 2, topTex.Height), color);
+            spriteBatch.Draw(topTex, new Vector2(pos.X + 2f, pos.Y), new Rectangle(2, 0, 2, topTex.Height), color, 0f, Vector2.Zero, new Vector2((width - 4f) / 2f, 1f), SpriteEffects.None, 0f);
+            spriteBatch.Draw(topTex, new Vector2(pos.X + width - 2f, pos.Y), new Rectangle(4, 0, 2, topTex.Height), color);
+
+            ChatManager.DrawColorCodedStringWithShadow(spriteBatch, FontAssets.ItemStack.Value, Text, pos + new Vector2(9, 5), Color.White, 0f, Vector2.Zero, textScale, width);
+
+            pos += new Vector2(0, 29f);
+
+            spriteBatch.Draw(bottomTex, pos, new Rectangle(0, 0, 6, bottomTex.Height), color);
+            spriteBatch.Draw(bottomTex, new Vector2(pos.X + 6f, pos.Y), new Rectangle(6, 0, 7, bottomTex.Height), color, 0f, Vector2.Zero, new Vector2((width - 12f) / 7f, 1f), SpriteEffects.None, 0f);
+            spriteBatch.Draw(bottomTex, new Vector2(pos.X + width - 6f, pos.Y), new Rectangle(13, 0, 6, bottomTex.Height), color);
+
+            pos += new Vector2(8, 4);
+            Vector2 subtextScale = new(0.92f);
+            string subtext = FontAssets.ItemStack.Value.CreateWrappedText(Subtext, (width - 20f) * (1f / subtextScale.X), Language.ActiveCulture.CultureInfo);
+            ChatManager.DrawColorCodedStringWithShadow(spriteBatch, FontAssets.ItemStack.Value, subtext, pos, Color.Gray, 0f, Vector2.Zero, subtextScale);
+        }
+    }
+    public class EbonianAchievementUIState : UIState
+    {
+        UIElement ui;
+        UIList achievementList;
+        List<EbonianAchievement> achievements = new List<EbonianAchievement>();
+        UIScrollbar scrollbar;
+        bool[] achievementActive = new bool[EbonianAchievementSystem.maxAchievements];
+        public override void OnInitialize()
+        {
+            achievements.Clear();
+            RemoveAllChildren();
+            ui = null;
+            achievementList = null;
+
+            ui = new UIElement();
+            ui.Width.Set(0f, 0.8f);
+            ui.MaxWidth.Set(900f, 0f);
+            ui.MinWidth.Set(700f, 0f);
+            ui.Top.Set(220f, 0f);
+            ui.Height.Set(-220f, 1f);
+            ui.HAlign = 0.5f;
+            Append(ui);
+
+            UIPanel uIPanel = new();
+            uIPanel.Width.Set(0f, 1f);
+            uIPanel.Height.Set(-110f, 1f);
+            uIPanel.BackgroundColor = new Color(51, 69, 132) * 0.75f;
+            uIPanel.PaddingTop = 0f;
+            ui.Append(uIPanel);
+
+            achievementList = new UIList();
+            achievementList.Width.Set(-25f, 1f);
+            achievementList.Height.Set(-50f, 1f);
+            achievementList.Top.Set(50f, 0f);
+            achievementList.ListPadding = 5f;
+            uIPanel.Append(achievementList);
+
+            UITextPanel<LocalizedText> uITextPanel = new(Language.GetText("UI.Achievements"), 1f, large: true)
+            {
+                HAlign = 0.5f
+            };
+            uITextPanel.Top.Set(-33f, 0f);
+            uITextPanel.SetPadding(13f);
+            uITextPanel.BackgroundColor = new Color(51, 69, 132) * 0.75f;
+            ui.Append(uITextPanel);
+
+            UITextPanel<LocalizedText> uITextPanel2 = new(Language.GetText("UI.Back"), 0.7f, large: true);
+            uITextPanel2.Width.Set(-10f, 0.5f);
+            uITextPanel2.Height.Set(50f, 0f);
+            uITextPanel2.VAlign = 1f;
+            uITextPanel2.HAlign = 0.5f;
+            uITextPanel2.Top.Set(-45f, 0f);
+            uITextPanel2.OnMouseOver += delegate (UIMouseEvent evt, UIElement listeningElement)
+            {
+                SoundEngine.PlaySound(SoundID.MenuTick);
+                ((UIPanel)evt.Target).BackgroundColor = new Color(51, 69, 132);
+                ((UIPanel)evt.Target).BorderColor = Colors.FancyUIFatButtonMouseOver;
+            };
+            uITextPanel2.OnMouseOut += delegate (UIMouseEvent evt, UIElement listeningElement)
+            {
+                ((UIPanel)evt.Target).BackgroundColor = new Color(51, 69, 132) * 0.75f;
+                ((UIPanel)evt.Target).BorderColor = Color.Black;
+            };
+            uITextPanel2.OnLeftClick += delegate (UIMouseEvent evt, UIElement listeningElement)
+            {
+                Main.menuMode = 0;
+                IngameFancyUI.Close();
+            };
+            uITextPanel2.BackgroundColor = new Color(51, 69, 132) * 0.75f;
+            ui.Append(uITextPanel2);
+
+            for (int i = 0; i < EbonianAchievementSystem.maxAchievements; i++)
+            {
+                EbonianAchievement achievement = new EbonianAchievement(i, EbonianAchievementSystem.achievementTemplates[i].Text, EbonianAchievementSystem.achievementTemplates[i].Subtext);
+                achievements.Add(achievement);
+                achievementList.Add(achievement);
+            }
+
+            scrollbar = new();
+            scrollbar.SetView(100f, 1000f);
+            scrollbar.Height.Set(-50f, 1f);
+            scrollbar.Top.Set(50f, 0f);
+            scrollbar.HAlign = 1f;
+            uIPanel.Append(scrollbar);
+
+            achievementList.SetScrollbar(scrollbar);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (!Main.inFancyUI)
+                return;
+            base.Draw(spriteBatch);
+        }
+        public override void OnActivate()
+        {
+
+        }
+        public override void Update(GameTime gameTime)
+        {
+
+            base.Update(gameTime);
+        }
+    }
+}
