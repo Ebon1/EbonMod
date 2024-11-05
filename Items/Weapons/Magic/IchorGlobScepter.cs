71cc479e2f4dbb3901b9e46563839d6871cd87c7
Ebon1 <alikhaddam42@gmail.com>
2024-11-05 19:53:07 +0100

    in hindsight setting the homing power to 100% wasnt that good of an idea

--- Items/Weapons/Magic/IchorGlobScepter.cs
+++ Items/Weapons/Magic/IchorGlobScepter.cs
@@ -188,7 +188,7 @@ namespace EbonianMod.Items.Weapons.Magic
         public override void AI()
         {
             if (Main.myPlayer == Projectile.owner)
-                Projectile.Center = Vector2.Lerp(Projectile.Center, Main.MouseWorld, (Projectile.ai[2]));
+                Projectile.Center = Vector2.Lerp(Projectile.Center, Main.MouseWorld, (Projectile.ai[2] * 0.05f));
             if (Projectile.timeLeft < 40)
                 Projectile.ai[2] = MathHelper.Lerp(Projectile.ai[2], 1, 0.007f);
             if (Projectile.timeLeft < 30)
