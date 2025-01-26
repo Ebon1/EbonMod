195cea0446943539b4130b34c55139e986a92c28
Ebon1 <alikhaddam42@gmail.com>
2025-01-26 18:28:35 +0100

    Shack gemlock no longer destroyable

--- Tiles/ModGlobalTile.cs
+++ Tiles/ModGlobalTile.cs
@@ -22,5 +22,30 @@ namespace EbonianMod.Tiles
                     Item.NewItem(Item.GetSource_NaturalSpawn(), new Vector2(i * 16, j * 16), ItemType<Potato>(), Main.rand.Next(1, 10));
             }
         }
+        public override bool CanKillTile(int i, int j, int type, ref bool blockDamaged)
+        {
+            if (type == TileID.GemLocks && Main.tile[i, j + 5].TileType == TileType<XHouseBrick>()) return false;
+            return base.CanKillTile(i, j, type, ref blockDamaged);
+        }
+        public override bool CanExplode(int i, int j, int type)
+        {
+            if (type == TileID.GemLocks && Main.tile[i, j + 5].TileType == TileType<XHouseBrick>()) return false;
+            return base.CanExplode(i, j, type);
+        }
+    }
+    public class EbonGlobalWall : GlobalWall
+    {
+        public override void KillWall(int i, int j, int type, ref bool fail)
+        {
+            if (Main.tile[i, j].TileType == TileID.GemLocks && Main.tile[i, j + 5].TileType == TileType<XHouseBrick>())
+                fail = true;
+            base.KillWall(i, j, type, ref fail);
+        }
+        public override bool CanExplode(int i, int j, int type)
+        {
+            if (Main.tile[i, j].TileType == TileID.GemLocks && Main.tile[i, j + 5].TileType == TileType<XHouseBrick>())
+                return false;
+            return base.CanExplode(i, j, type);
+        }
     }
 }
