678403dd1af9088877c03e53cf3c24074dc504bf
Ebon1 <alikhaddam42@gmail.com>
2025-01-26 21:13:36 +0100

    I HATE THIS FUCKING GAME

--- NPCs/ArchmageX/ArchmageStaffNPC.cs
+++ NPCs/ArchmageX/ArchmageStaffNPC.cs
@@ -146,11 +146,11 @@ namespace EbonianMod.NPCs.ArchmageX
             {
                 if (!GetInstance<EbonianSystem>().gotTheStaff)
                 {
-                    if (NPC.ai[1] < 1960)
+                    if (NPC.ai[3] < 1960)
                         staffAlpha = Lerp(staffAlpha, 1, 0.1f);
-                    if (dist < 300 && p.Player.Center.Y.CloseTo(NPC.Center.Y - 30, 100) && NPC.ai[1] <= 0)
-                        NPC.ai[1] = 1;
-                    if (NPC.ai[1] > 0)
+                    if (dist < 300 && p.Player.Center.Y.CloseTo(NPC.Center.Y - 30, 100) && NPC.ai[3] <= 0)
+                        NPC.ai[3] = 1;
+                    if (NPC.ai[3] > 0)
                     {
                         if (GetArenaRect().Size().Length() > 100)
                         {
@@ -171,7 +171,7 @@ namespace EbonianMod.NPCs.ArchmageX
                             }
                         }
                     }
-                    if (NPC.ai[1] > 0)
+                    if (NPC.ai[3] > 0)
                         NPC.ai[1]++;
                     if (NPC.ai[1] == 100)
                         d = DialogueSystem.NewDialogueBox(60, NPC.Center - new Vector2(0, 60), "...", Color.White, -1, 0.6f, Color.Magenta * 0.6f, 1.75f, true, DialogueAnimationIDs.BopDown | DialogueAnimationIDs.ColorWhite, SoundID.DD2_CrystalCartImpact.WithPitchOffset(0.9f), 2);
