332bdd47a505efde2c53d66d797dfd12439883f9
Ebon1 <alikhaddam42@gmail.com>
2025-01-27 11:36:53 +0100

    Bloated Ebonflies now finish their explosion animation if it already started

--- NPCs/Corruption/Ebonflies/BloatedEbonfly.cs
+++ NPCs/Corruption/Ebonflies/BloatedEbonfly.cs
@@ -111,7 +111,7 @@ namespace EbonianMod.NPCs.Corruption.Ebonflies
                     }
                 }
             }
-            if (Main.LocalPlayer.Center.Distance(NPC.Center) < 450 || (NPC.dontTakeDamage && ++AITimer > 60 * 8))
+            if (Main.LocalPlayer.Center.Distance(NPC.Center) < 450 || (NPC.dontTakeDamage && ++AITimer > 60 * 8) || NPC.ai[3] > 0)
             {
                 if (++NPC.ai[3] > 100 * NPC.scale)
                 {
