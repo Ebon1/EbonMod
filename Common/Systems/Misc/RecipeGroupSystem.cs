c98ef67bdc1cfcd1fa839b25857b16f3c7d3b387
Quarzz <130183472+Quarzz1@users.noreply.github.com>
2025-02-27 17:46:00 +0200

    rerouted recipegroup code

--- Common/Systems/Misc/RecipeGroupSystem.cs
+++ Common/Systems/Misc/RecipeGroupSystem.cs
@@ -9,9 +9,19 @@ namespace EbonianMod.Common.Systems.Misc
     public class RecipeGroupSystem : ModSystem
     {
         public static RecipeGroup SilverBars;
+
+        public override void Unload()
+        {
+            SilverBars = null;
+        }
+        
         public override void AddRecipeGroups()
         {
-            // SilverBars = new RecipeGroup(() => "SilverOrTungsten", [ItemID.SilverBar, ItemID.TungstenBar]);
+            SilverBars = new RecipeGroup(() => $"{Language.GetTextValue("LegacyMisc.37")} {Lang.GetItemNameValue(ItemID.SilverBar)}",
+                ItemID.SilverBar, ItemID.TungstenBar);
+            RecipeGroup.RegisterGroup("EbonianMod:AnySilver", SilverBars);
+
+        
         }
     }
 }
