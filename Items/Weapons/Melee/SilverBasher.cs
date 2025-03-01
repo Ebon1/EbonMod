3716fd2105e558f663980724e07a0483bc20d8fe
Ebon1 <alikhaddam42@gmail.com>
2025-03-01 14:28:11 +0100

    silver basher rework

--- Items/Weapons/Melee/SilverBasher.cs
+++ Items/Weapons/Melee/SilverBasher.cs
@@ -20,17 +20,18 @@ namespace EbonianMod.Items.Weapons.Melee
             Item.noUseGraphic = true;
             Item.consumable = false;
             Item.Size = new(20);
-            Item.useAnimation = 50;
+            Item.useAnimation = 20;
             Item.crit = 15;
-            Item.useTime = 50;
+            Item.useTime = 20;
             Item.DamageType = DamageClass.Melee;
             Item.damage = 73;
+            Item.reuseDelay = 30;
             Item.UseSound = SoundID.Item1;
             Item.autoReuse = true;
             Item.value = Item.buyPrice(0, 8, 0, 0);
             Item.shoot = ProjectileType<SilverBasherP>();
             Item.rare = ItemRarityID.LightRed;
-            Item.shootSpeed = 15f;
+            Item.shootSpeed = 10f;
         }
         public override void AddRecipes()
         {
@@ -38,7 +39,7 @@ namespace EbonianMod.Items.Weapons.Melee
         }
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, ai1: 0);
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, ai1: 1.4f);
             return false;
         }
     }
@@ -57,7 +58,8 @@ namespace EbonianMod.Items.Weapons.Melee
             Projectile.DamageType = DamageClass.Melee;
             Projectile.penetrate = -1;
             Projectile.usesLocalNPCImmunity = true;
-            Projectile.timeLeft = 150;
+            Projectile.timeLeft = 120;
+            Projectile.extraUpdates = 2;
             Projectile.tileCollide = true;
 
         }
@@ -67,19 +69,12 @@ namespace EbonianMod.Items.Weapons.Melee
         }
         public override void AI()
         {
-            Lighting.AddLight(Projectile.Center, TorchID.Torch);
-            Player player = Main.player[Projectile.owner];
-            foreach (Projectile projectile in Main.ActiveProjectiles)
-            {
-                if (Projectile.Distance(projectile.Center) < 45 && projectile != Projectile && Type == projectile.type)
-                {
-                    Projectile.velocity = Helper.FromAToB(Projectile.Center, projectile.Center) * -projectile.velocity.Length() * 1.3f;
-                    projectile.velocity *= 0.14f;
-                }
-            }
-            Projectile.velocity = Vector2.Lerp(Projectile.velocity, new Vector2(0, 0), 0.05f);
+            if (Projectile.ai[2] == 0)
+                Projectile.ai[2] = Projectile.velocity.Length();
+            Lighting.AddLight(Projectile.Center, TorchID.Red);
+            Projectile.velocity = Vector2.Lerp(Projectile.velocity, Vector2.Zero, 0.025f);
 
-            Projectile.ai[1] = Lerp(Projectile.ai[1], 0f, 0.1f);
+            Projectile.ai[1] = Lerp(Projectile.ai[1], 0f, 0.01f);
             Projectile.rotation += Projectile.ai[1];
         }
         public override bool OnTileCollide(Vector2 oldVelocity)
@@ -89,6 +84,15 @@ namespace EbonianMod.Items.Weapons.Melee
         }
         public override void OnKill(int timeLeft)
         {
+            Vector2 vel = Projectile.velocity.SafeNormalize(Vector2.Zero) * Projectile.ai[2] * 0.5f;
+            if (Projectile.ai[0] == 0)
+            {
+                for (int i = -1; i < 2; i++)
+                {
+                    float angle = i * 0.7f;
+                    Projectile.NewProjectile(Projectile.InheritSource(Projectile), Projectile.Center, vel.RotatedBy(angle), Type, Projectile.damage, Projectile.knockBack, Projectile.owner, 1, 1.4f);
+                }
+            }
             Projectile.NewProjectile(null, Projectile.Center, Vector2.Zero, ProjectileType<SilverBasherFlash>(), 0, 0);
             Projectile Proj = Projectile.NewProjectileDirect(Projectile.InheritSource(Projectile), Projectile.Center, Vector2.Zero, ProjectileType<FlameExplosionWSprite>(), Projectile.damage, 100, Projectile.owner);
             Proj.friendly = true;
